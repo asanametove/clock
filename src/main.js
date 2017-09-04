a9979ba4f193fa33d98a49f6a0093ea0d7a65f96
Maksym Pushkarov <Maksym_Pushkarov@emap.com>
2017-09-04 10:04:57 +0300

    clock with current time

--- src/main.js
+++ src/main.js
@@ -1,17 +1,14 @@
 'use strict';
 
 window.onload = function () {
-    let curTime = new Time(0,0,0);
-    let secArrow = document.querySelector('.arrow-second');
-    let minArrow = document.querySelector('.arrow-minute');
-    let hourArrow = document.querySelector('.arrow-hour');
+    let date = new Date();
+    let curTime = new Time(+date.getHours() + date.getMinutes()/60 ,+date.getMinutes(),+date.getSeconds());
+    setArrowsData(curTime);
 
     setInterval(function () {
         curTime.tick();
-        secArrow.style.transform = `rotate(${(curTime.get().second * 6)}deg)`;
-        minArrow.style.transform = `rotate(${(curTime.get().minute * 6)}deg)`;
-        hourArrow.style.transform = `rotate(${(curTime.get().hour * 30)}deg)`;
-    }, 10);
+        setArrowsData(curTime);
+    }, 1000);
 
     function Time(hour, minute, second) {
         return {
@@ -21,13 +18,13 @@ window.onload = function () {
 
         function tick() {
             second++;
-            console.log(second);
+            // console.log(second);
             if(second === 60) {
                 second = 0;
                 minute++;
+                hour += 1/60;
                 if(minute === 60) {
                     minute = 0;
-                    hour++;
                     if(hour === 12) {
                         hour = 0;
                     }
@@ -44,3 +41,13 @@ window.onload = function () {
         }
     }
 };
+
+function setArrowsData(curTime) {
+    let secArrow = document.querySelector('.arrow-second');
+    let minArrow = document.querySelector('.arrow-minute');
+    let hourArrow = document.querySelector('.arrow-hour');
+
+    secArrow.style.transform = `rotate(${(curTime.get().second * 6)}deg)`;
+    minArrow.style.transform = `rotate(${(curTime.get().minute * 6)}deg)`;
+    hourArrow.style.transform = `rotate(${(curTime.get().hour * 30)}deg)`;
+}
