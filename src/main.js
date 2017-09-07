3cca1f5447a16e6854efb38c121faa8498bf632c
Maksym Pushkarov <Maksym_Pushkarov@emap.com>
2017-09-07 14:17:59 +0300

    Clock finished

--- src/main.js
+++ src/main.js
@@ -1,53 +1,114 @@
 'use strict';
 
-window.onload = function () {
-    let date = new Date();
-    let curTime = new Time(+date.getHours() + date.getMinutes()/60 ,+date.getMinutes(),+date.getSeconds());
-    setArrowsData(curTime);
 
+(function (window) {
+  function Clock() {
+    init();
+  }
+
+  function init() {
+    //initialization arrows
+    let {secArrow,
+      minArrow,
+      hourArrow,
+      elementsClockFace,
+      ClockWrap}  = initialElement();
+
+    //set current time
+    setTime(secArrow, minArrow, hourArrow);
+
+    //set clock face on clock
+    setClockFace(ClockWrap, elementsClockFace);
+
+
+    //run dinamical clock
     setInterval(function () {
-        curTime.tick();
-        setArrowsData(curTime);
+      curTime.tick();
+      setTime(secArrow, minArrow, hourArrow);
     }, 1000);
+  }
 
-    function Time(hour, minute, second) {
-        return {
-            get: get,
-            tick: tick
-        };
+  let date = new Date();
+  let currentHour = date.getHours() + date.getMinutes() / 60;
+  let currentMinute = date.getMinutes();
+  let currentSecond = date.getSeconds();
+  let curTime = new Time(currentHour, currentMinute, currentSecond);
 
-        function tick() {
-            second++;
-            // console.log(second);
-            if(second === 60) {
-                second = 0;
-                minute++;
-                hour += 1/60;
-                if(minute === 60) {
-                    minute = 0;
-                    if(hour === 12) {
-                        hour = 0;
-                    }
-                }
-            }
+  function setTime(sec, min, hour) {
+    sec.style.transform = `rotate(${(curTime.get().second * 6)}deg)`;
+    min.style.transform = `rotate(${(curTime.get().minute * 6)}deg)`;
+    hour.style.transform = `rotate(${(curTime.get().hour * 30)}deg)`;
+  }
+
+  function Time(hour, minute, second) {
+    return {
+      get,
+      tick
+    };
+
+    function tick() {
+      second++;
+      if (second === 60) {
+        second = 0;
+        minute++;
+        hour += 1 / 60;
+        if (minute === 60) {
+          minute = 0;
+          if (hour === 12) {
+            hour = 0;
+          }
         }
-
-        function get() {
-            return {
-                hour: hour,
-                minute: minute,
-                second: second
-            }
-        }
+      }
     }
-};
 
-function setArrowsData(curTime) {
-    let secArrow = document.querySelector('.arrow-second');
-    let minArrow = document.querySelector('.arrow-minute');
-    let hourArrow = document.querySelector('.arrow-hour');
+    function get() {
+      return {
+        hour: hour,
+        minute: minute,
+        second: second
+      }
+    }
+  }
 
-    secArrow.style.transform = `rotate(${(curTime.get().second * 6)}deg)`;
-    minArrow.style.transform = `rotate(${(curTime.get().minute * 6)}deg)`;
-    hourArrow.style.transform = `rotate(${(curTime.get().hour * 30)}deg)`;
-}
+  function setClockFace(clockWrap, elementsClockFace) {
+    const RADIUS = clockWrap.offsetWidth / 2;
+    let elementsClockFaceLength = elementsClockFace.length,
+      x0 = RADIUS,
+      y0 = 0,
+      x = RADIUS,
+      y = RADIUS,
+      rX = x0 - x,
+      rY = y0 - y,
+      fullCicle = 360,
+      step = fullCicle / elementsClockFaceLength,
+      deg = 0.0174532925;
+
+    for (let i = 0; i < elementsClockFaceLength; i++) {
+      let c = Math.cos((step * i + step) * deg);
+      let s = Math.sin((step * i + step) * deg);
+      let x1 = (x + rX * c) - (rY * s);
+      let y1 = (y + rX * s) + (rY * c);
+      elementsClockFace[i].style.top = y1 + 'px';
+      elementsClockFace[i].style.left = x1 + 'px';
+    }
+  }
+
+  function initialElement() {
+    let secArrow = document.querySelector('.arrow-second'),
+      minArrow = document.querySelector('.arrow-minute'),
+      hourArrow = document.querySelector('.arrow-hour'),
+      elementsClockFace = document.getElementsByClassName('hour'),
+      ClockWrap = document.querySelector('.clock');
+    return {
+      secArrow,
+      minArrow,
+      hourArrow,
+      elementsClockFace,
+      ClockWrap
+
+    }
+  }
+
+
+  window.clock = new Clock();
+})(window);
